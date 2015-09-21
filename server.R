cbc371e88ccec2e84539164e68a1bdc06dbdbd76
Tokhir Dadaev <zx8754@gmail.com>
2015-09-21 14:57:57 +0100

    old version

---
+++ server.R
@@ -0,0 +1,72 @@
+shinyServer(
+  function(input, output, session) {
+    
+    #if Clear pressed then clear the text box
+    observe({
+      if (input$clearText_button == 0) return()
+      isolate({ updateTextInput(session, "PasteData", label = ",", value = "") })
+    })
+    
+    #Input Data Tab --------------------------------------------------------------
+    #Convert pasted text into a dataframe
+    datPSA <- reactive({
+      if(is.null(input$PasteData)) {return(NULL)}
+      mySep<-switch(input$fileSepP, '1'=",",'2'="\t",'3'=";")
+      x <- matrix(strsplit(input$PasteData, "\n")[[1]])
+      x <- do.call(rbind,lapply(x,function(i){strsplit(i,mySep)[[1]]}))
+      x <- data.frame(SampleID=x[,1],
+                      PSA_Date=x[,2],
+                      PSA=as.numeric(x[,3]),stringsAsFactors = FALSE)
+    })
+    
+    #Get results using udf_PSA_velocity function
+    datPSAResult <- reactive({udf_PSA_velocity(datPSA())})
+    
+    #Show input data as table on "Input Data" tab
+    output$PSAV <- renderTable({
+      datPSA()
+    })
+    
+    #Result Tab --------------------------------------------------------------
+    #Output for "Result" tab
+    output$PSAV_result <- renderTable({
+      datPSAResult()
+    }, digits=4)
+    
+    #Option to download Results
+    output$downloadResult <- downloadHandler(
+      filename = function() { 
+        "PSAV_results.csv" 
+      },
+      content = function(file) {
+        write.csv(datPSAResult(), file,row.names=FALSE)})
+    
+    #Plot Tab --------------------------------------------------------------
+    #Dynamic input - Select Sample for plot
+    output$SampleID <- renderUI({
+      selectInput("SampleID", strong("Sample ID:"),
+                  choices = sort(unique(datPSA()$SampleID)))
+    })
+    
+    ##plot for selected sample
+    output$SamplePlot <- renderPlot({
+      d <- datPSA()[ datPSA()$SampleID == input$SampleID,]
+      d$PSA_Date <- as.Date(d$PSA_Date,"%d/%m/%Y")
+      d$FirstLast <- ifelse(d$PSA_Date %in% c(min(d$PSA_Date,na.rm=TRUE),
+                                              max(d$PSA_Date,na.rm=TRUE)),1,0)
+      ggplot(d,
+             aes(x=PSA_Date,y=PSA)) +
+        geom_point() +
+        geom_line(colour="green") +
+        geom_smooth(method=lm,se=FALSE) +
+        geom_line(data=d[ d$FirstLast==1,], aes(x=PSA_Date,y=PSA),col="red") +
+        theme_classic()
+      
+    })
+    output$PSAV_result_selected <- renderTable({
+      datPSAResult()[datPSAResult()$SampleID==input$SampleID,]
+    }, digits=4)
+    
+    
+  }
+)#shinyServer
---
+++ ui.R
@@ -0,0 +1,62 @@
+library(shiny)
+library(shinyAce)
+library(ggplot2)
+source("udf_PSA_velocity.R")
+
+shinyUI(
+  
+  pageWithSidebar(
+    headerPanel(h3("PSA Velocity - v0.1"),windowTitle="PSA Velocity"),
+    
+    sidebarPanel(
+      h5("Paste data below:"),
+      
+      #Example input data
+      tags$textarea(id="PasteData", rows=10, cols=50, placeholder="SampleID,PSA_Date,PSA",
+"Sample_1,19/09/2008,0.77
+Sample_1,15/01/2010,0.48
+Sample_1,11/03/2011,0.7
+Sample_1,23/03/2012,0.53
+Sample_2,19/09/2008,0.77
+Sample_2,15/01/2010,0.48
+Sample_3,11/03/2011,0.7"),
+      p(),
+      actionButton('clearText_button','Clear data'),
+      p(),
+      #Choose Field separator
+      radioButtons("fileSepP", h5("Field Separator:"), list("Comma"=1,"Tab"=2,"Semicolon"=3))
+      ),#pageWithSidebar
+    
+    mainPanel(
+      tabsetPanel(type = "pills",
+                  tabPanel("Input Data",tableOutput("PSAV")),
+                  tabPanel("Result",
+                           tableOutput("PSAV_result"),
+                           downloadButton("downloadResult", "Download")),
+                  tabPanel("Plot",
+                           uiOutput("SampleID"),
+                           plotOutput("SamplePlot"),
+                           tableOutput("PSAV_result_selected")),
+                  tabPanel("Help",
+                           #withMathJax(),
+                           #helpText('An irrational number \\(\\sqrt{2}\\) and a fraction $$1-\\frac{1}{2}$$'),
+                           HTML("<p><p>Under construction...<p><p><p>"),
+                           HTML('<p>This application was developed with XYZ journal Methods as described in this <a href="http://www.ncbi.nlm.nih.gov/pubmed">editorial</a>. <p> The application allows users to calculate PSA velocity using <a href="http://www.ncbi.nlm.nih.gov/pubmed/17197071">three methods</a> from Connolly D paper:<p>'),
+                           helpText("1. Arithmetic equation of change in PSA over time (AE)"),
+                           helpText("2. Linear regression (LR)"),
+                           helpText("3. Rate of PSA change using first and last values only (FL)"),
+                           HTML('<p>A data matrix can be pasted into the application.
+                                <p>PSA Velocity code can be run locally via <a href="https://github.com/zx8754/PSA_Velocity">GitHub</a>.'),
+                           h5("Software references"),
+                           HTML('<p>R Development Core Team. <i><a href="http://www.r-project.org/">R</a>:  A Language and Environment for Statistical Computing.</i> R Foundation for Statistical Computing, Vienna (2013) <br>
+                                RStudio and Inc. <i><a href="http://www.rstudio.com/shiny/">shiny</a>: Web Application Framework for R.</i> R package version 0.5.0 (2013) <br>'),
+                           h5("Further references"),
+                           HTML('<p> Connolly D1, Black A, Murray LJ, Napolitano G, Gavin A, Keane PF: <a href="http://www.ncbi.nlm.nih.gov/pubmed/17197071"> Methods of calculating prostate-specific antigen velocity. </a></p>'),
+                           h6("This application was created by the ",
+                              a("T.Dadaev", href="https://twitter.com/zx8754"), " and ",
+                              a("Prof. R. Eeles", href="http://www.icr.ac.uk/our-research/researchers-and-teams/professor-rosalind-eeles"),
+                              " labs."))
+                           )#tabsetPanel
+    )#mainPanel
+  )#sidebarPanel
+  )
